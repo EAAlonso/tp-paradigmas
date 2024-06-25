ab30525b0b39ae29b9ea9ac94cb5746958848cd3
Ignacio Collazo <105324633+IgnaColla@users.noreply.github.com>
2024-06-25 16:44:17 -0300

    Update Usuario.java

--- src/cryptomanager/Usuario.java
+++ src/cryptomanager/Usuario.java
@@ -26,7 +26,7 @@ public abstract class Usuario {
 	
 	protected void consultarCriptomoneda() {
 		Menu.limpiarConsola();
-		System.out.println("===== Consulta de criptomoneda ===== ");
+		System.out.println("+----- CONSULTAR CRIPTOMONEDA -----+");
 		
 		String nombre = this.pedirNombreDeCriptomonedaExistente();
 		if (nombre == null) { // Cancela la operacion
@@ -35,20 +35,20 @@ public abstract class Usuario {
 		
 		System.out.println(this.criptomonedas.obtenerRegistro(nombre));
 		
-		System.out.println("Presione una tecla para continuar...");
+		System.out.println(" \n+ Presione una tecla para continuar...");
 		Menu.esperarTecla();
 	}
 	
 	protected String pedirNombreDeCriptomonedaExistente() {
-		String nombre = Menu.pedirString("Ingrese el nombre de la criptomoneda: ");
+		String nombre = Menu.pedirString("+ Ingrese el nombre de la criptomoneda: ");
 
 		while (!this.criptomonedas.existe(nombre)) {
-			boolean reintentar = Menu.pedirYoN("Criptomoneda no encontrada, ¿volver a intentar? Y (Si) / N (No): ");
+			boolean reintentar = Menu.pedirYoN("+ Criptomoneda no encontrada. ¿Volver a intentar?\n- Y (Si)\n- N (No)\nIngrese opcion: ");
 			if (!reintentar) {
 				return null;
 			}
 
-			nombre = Menu.pedirString("Ingrese el nombre de la criptomoneda: ");
+			nombre = Menu.pedirString("+ Ingrese el nombre de la criptomoneda: ");
 		}
 		
 		return nombre;
@@ -56,12 +56,12 @@ public abstract class Usuario {
 	
 	protected void consultarMercado() {
 		Menu.limpiarConsola();
-		System.out.println(" ======== ESTADO DEL MERCADO ========== ");
+		System.out.println("+----- ESTADO DEL MERCADO -----+");
 		for(String simboloCripto : this.mercado.getContenido().keySet()) {
 			System.out.println(this.mercado.obtenerRegistro(simboloCripto));
 		}
 		
-		System.out.println("Presione una tecla para continuar...");
+		System.out.println("\n+ Presione una tecla para continuar...");
 		Menu.esperarTecla();
 	}
 }
